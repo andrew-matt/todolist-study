9c93dcd653db9e40b1d3ad06b754c84a964927f9
andrew-matt <andrew.matt@ya.ru>
2022-04-18 16:14:57 +0300

    update todolistsReducerTest

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
 type ActionType = {
     type: string
@@ -9,6 +10,11 @@ export const todoListsReducer = (state: TodolistType[], action: ActionType): Tod
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(tl => tl.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [
+                ...state,
+                {id: v1(), title: action.title, filter: "all"}
+            ]
         default:
             throw new Error("I don't understand this type")
     }
