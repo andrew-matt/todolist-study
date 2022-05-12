4613b2006ade6de0d47b37d20ca389a5ed38a258
andrew-matt <andrew.matt@ya.ru>
2022-05-12 18:21:09 +0300

    add useReducers

--- src/state/todolists-tasks-reducer.test.ts
+++ src/state/todolists-tasks-reducer.test.ts
@@ -1,13 +1,12 @@
-import {TasksStateType, TodolistType} from "../App";
-import {addTodolistAC, todolistsReducer} from "./todolists-reducer";
-import {tasksReducer} from "./tasks-reducer";
+import {TasksStateType, TodolistType} from '../App';
+import {addTodolistAC, todolistsReducer} from './todolists-reducer';
+import {tasksReducer} from './tasks-reducer';
 
 test('ids should be equals', () => {
     const startTasksState: TasksStateType = {};
     const startTodolistsState: Array<TodolistType> = [];
 
     const action = addTodolistAC("new todolist");
-
     const endTasksState = tasksReducer(startTasksState, action)
     const endTodolistsState = todolistsReducer(startTodolistsState, action)
 
@@ -18,6 +17,3 @@ test('ids should be equals', () => {
     expect(idFromTasks).toBe(action.todolistId);
     expect(idFromTodolists).toBe(action.todolistId);
 });
-
-
-
