e36373ae6e4cb41f60fbf6c9bc65291afb7a6970
andrew-matt <andrew.matt@ya.ru>
2022-05-29 16:56:48 +0300

    add decorators

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,14 +1,16 @@
 import AppWithRedux from "./AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
+import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
 export default {
     title: 'AppWithRedux Component',
     component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator],
 }
 
 export const AppWithReduxBaseExample = () => {
     return (
-        <Provider store={store}><AppWithRedux/></Provider>
+        <AppWithRedux/>
     )
 }
