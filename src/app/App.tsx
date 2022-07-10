c46cb0076dc26158b28c064c57f8ac2378b50eab
andrew-matt <andrew.matt@ya.ru>
2022-07-10 16:10:27 +0300

    step 16

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import './App.css';
-import {TodolistsList} from '../features/TodolistsList/TodolistsList';
-
-// You can learn about the difference by reading this guide on minimizing bundle size.
-// https://mui.com/guides/minimizing-bundle-size/
-// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import React from 'react'
+import './App.css'
+import { TodolistsList } from '../features/TodolistsList/TodolistsList'
+import { useSelector } from 'react-redux'
+import { AppRootStateType } from './store'
+import { RequestStatusType } from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import {Menu} from '@mui/icons-material';
-import {LinearProgress} from '@mui/material';
-import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
-import {useSelector} from 'react-redux';
-import {AppRootStateType} from './store';
+import LinearProgress from '@mui/material/LinearProgress';
+import { Menu } from '@mui/icons-material';
+import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
 
+type PropsType = {
+    demo?: boolean
+}
 
-function App() {
-
-    const status = useSelector<AppRootStateType>(state => state.app.status);
-
+function App({demo = false}: PropsType) {
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
     return (
         <div className="App">
             <ErrorSnackbar/>
@@ -35,15 +33,13 @@ function App() {
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
-                {
-                    status === 'loading' && <LinearProgress/>
-                }
+                {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList/>
+                <TodolistsList demo={demo}/>
             </Container>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
