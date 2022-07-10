99b41724db9afad0d1a48ae40dbccc6757061072
andrew-matt <andrew.matt@ya.ru>
2022-07-10 17:19:37 +0300

    add Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './App.css'
-import { TodolistsList } from '../features/TodolistsList/TodolistsList'
-import { useSelector } from 'react-redux'
-import { AppRootStateType } from './store'
-import { RequestStatusType } from './app-reducer'
+import React from 'react';
+import './App.css';
+import {TodolistsList} from '../features/TodolistsList/TodolistsList';
+import {useSelector} from 'react-redux';
+import {AppRootStateType} from './store';
+import {RequestStatusType} from './app-reducer';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -11,35 +11,44 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
-import { Menu } from '@mui/icons-material';
-import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import {Login} from '../features/Login/Login';
 
 type PropsType = {
     demo?: boolean
 }
 
 function App({demo = false}: PropsType) {
-    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status);
     return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-                {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                <TodolistsList demo={demo}/>
-            </Container>
-        </div>
-    )
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Menu/>
+                        </IconButton>
+                        <Typography variant="h6">
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                    {status === 'loading' && <LinearProgress/>}
+                </AppBar>
+                <Container fixed>
+                    <Routes>
+                        <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                        <Route path="login" element={<Login/>}/>
+                        <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>}/>
+                        <Route path="*" element={<Navigate to={'/404'}/>}/>
+                    </Routes>
+                </Container>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
